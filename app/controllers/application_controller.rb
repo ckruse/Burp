279e9f370b05425d8a1d5e6400de76bf6f498be6
Christian Kruse <cjk@defunct.ch>
2015-02-26 14:33:17 +0100

    allow http authenticated requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  skip_before_action :verify_authenticity_token, if: :json_request?
 
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
@@ -68,4 +69,7 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) << :username
   end
 
+  def json_request?
+    request.format.json? and env['action_dispatch.http_auth_salt'] == 'http authentication'
+  end
 end
