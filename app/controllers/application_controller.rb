25bda206306a9fd53c6942ed3c63898636ff8031
Christian Kruse <cjk@defunct.ch>
2016-12-12 08:55:53 +0100

    fix: use new devise api

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -80,7 +80,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email])
   end
 
   def json_request?
