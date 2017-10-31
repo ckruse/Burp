a9a37f8bbce65964ac6d7de6d74b98a5a88ff73b
Christian Kruse <christian@kruse.cool>
2017-10-31 10:02:44 +0100

    ref: detail work

--- lib/burp_web/views/admin/comment_view.ex
+++ lib/burp_web/views/admin/comment_view.ex
@@ -3,4 +3,10 @@ defmodule BurpWeb.Admin.CommentView do
 
   def has_subnav?(_, _), do: true
   def subnav(_, conn, assigns), do: render("subnav.html", Map.put(assigns, :conn, conn))
+
+  def comment_url(conn, comment),
+    do: BurpWeb.PostView.posting_url(conn, comment.post) <> "#comment-#{comment.id}"
+
+  def comment_url(conn, comment, post),
+    do: BurpWeb.PostView.posting_url(conn, post) <> "#comment-#{comment.id}"
 end
