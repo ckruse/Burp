483e21a49b9c18c9830d42bd5f44571f7d696b04
Christian Kruse <christian@kruse.cool>
2017-11-05 10:52:22 +0100

    ref: send a 7 day cache control and expires header

--- lib/burp_web/controllers/medium_controller.ex
+++ lib/burp_web/controllers/medium_controller.ex
@@ -7,7 +7,12 @@ defmodule BurpWeb.MediumController do
     medium = Media.get_medium_by_slug!(slug)
 
     conn
-    |> put_resp_header("Content-Type", medium.media_type)
+    |> put_resp_header("content-type", medium.media_type)
+    |> put_resp_header("cache-control", "public, max-age=604800")
+    |> put_resp_header(
+         "expires",
+         Timex.now() |> Timex.shift(days: 7) |> Timex.format!("{RFC1123}")
+       )
     |> send_file(200, Media.filename(medium))
   end
 end
