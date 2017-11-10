987c0f4bf9e4674456ad723b6188c660694b8fbd
Christian Kruse <cjk@defunct.ch>
2017-11-10 10:10:34 +0100

    fix: listen on port 4001

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Burp.Mixfile do
   def project do
     [
       app: :burp,
-      version: "0.0.1",
+      version: "0.0.2",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
