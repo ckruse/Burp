334995a64d0584afbad7fd132e606ab29500723a
Christian Kruse <cjk@defunct.ch>
2020-04-25 07:13:49 +0200

    version 0.0.14

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Burp.Mixfile do
   def project do
     [
       app: :burp,
-      version: "0.0.13",
+      version: "0.0.14",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
