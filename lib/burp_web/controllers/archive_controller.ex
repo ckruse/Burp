44ca6db752aea3043525675a739dd7315d4aa39f
Christian Kruse <cjk@defunct.ch>
2020-04-29 08:53:25 +0200

    fix: fail on invalid month

--- lib/burp_web/controllers/archive_controller.ex
+++ lib/burp_web/controllers/archive_controller.ex
@@ -16,14 +16,21 @@ defmodule BurpWeb.ArchiveController do
 
   def index(conn, %{"year" => year_str, "mon" => month_str} = _params) do
     year = year_as_int(conn, year_str)
-    month = from_short(month_str)
+    month = from_short(conn, month_str)
     posts = Blog.list_posts_by_year_and_month(year, month, conn.assigns[:current_blog])
 
     render(conn, "index.html", posts: posts, month: Timex.beginning_of_month(year, month))
   end
 
   @months [nil, "jan", "feb", "mar", "apr", "may", "jun", "jul", "aug", "sep", "oct", "nov", "dec"]
-  defp from_short(month_name), do: Enum.find_index(@months, &(&1 == month_name))
+  defp from_short(conn, month_name) do
+    month = Enum.find_index(@months, &(&1 == month_name))
+
+    if is_nil(month),
+      do: raise(Burp.NotFoundError, conn: conn)
+
+    month
+  end
 
   defp year_as_int(conn, year) do
     if Regex.match?(~r/^\d+$/, year),
