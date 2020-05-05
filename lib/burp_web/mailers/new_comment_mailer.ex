a1cdc9cf59ccb7b69c8b41786b61923d950c15ec
Christian Kruse <cjk@defunct.ch>
2020-05-05 08:00:19 +0200

    fix: corrected syntax for sender

--- lib/burp_web/mailers/new_comment_mailer.ex
+++ lib/burp_web/mailers/new_comment_mailer.ex
@@ -4,7 +4,7 @@ defmodule BurpWeb.NewCommentMailer do
 
   def new_comment_mail(post, comment) do
     new()
-    |> from("Burp <christian@kruse.cool>")
+    |> from({"Burp", "christian@kruse.cool"})
     |> to(post.blog.attrs["mail_notify"])
     |> subject(gettext("A new comment has been posted"))
     |> render_body(:new_comment_mail, post: post, comment: comment)
